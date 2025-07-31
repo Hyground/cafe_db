9723cf9f89a5cd2f16b4e22bc9dd5fad85e5c761
Hyground <hygroundd@gmail.com>
2025-07-31 13:57:33 -0600

    primeros

---
+++ src/main/java/com/cafeumg/cdfs/CdfsApplication.java
@@ -0,0 +1,13 @@
+package com.cafeumg.cdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafeumg/cdfs/CdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeumg.cdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
